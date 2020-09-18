bff0176cdc41e86c217aef8aaffee0e73c8c5f0d
Dzuchun <Dzuchun@192.168.0.30>
2020-09-18 17:27:32 +0100

    (Over)writed delayed messages system. Added rDel command

---
+++ src/dzuchun/kyobot/DelayedMessage.java
@@ -0,0 +1,83 @@
+package dzuchun.kyobot;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dzuchun.util.SavedObject;
+
+public class DelayedMessage implements SavedObject {
+	private static final Logger LOGGER = LoggerFactory.getILoggerFactory().getLogger("Del message");
+
+	public DelayedMessage() {
+	}
+
+	public DelayedMessage(long publicationTimeIn, String channelNameIn, String messageIn, Type typeIn) {
+		this.publicationTime = publicationTimeIn;
+		this.channelName = channelNameIn;
+		this.message = messageIn;
+		this.type = typeIn;
+		LOGGER.info("Creating delayed message to publicate at time {}", publicationTime);
+	}
+
+	private long publicationTime;
+
+	public long getPublicationTime() {
+		return publicationTime;
+	}
+
+	private String channelName;
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	private String message; // TODO change this, so it supports unsupported yet stuff
+
+	public String getMessage() {
+		return message;
+	}
+
+	private Type type;
+
+	public Type getType() {
+		return type;
+	}
+
+	@Override
+	public void read(ObjectInputStream streamIn) throws IOException {
+		this.publicationTime = streamIn.readLong();
+		try {
+			this.channelName = (String) streamIn.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			this.message = (String) streamIn.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			LOGGER.error("This may signalise, that you don't have JDA library properly installed.");
+		}
+		this.type = streamIn.readInt() == 0 ? Type.DELAYED : Type.SCHEDULED;
+	}
+
+	@Override
+	public void save(ObjectOutputStream streamIn) throws IOException {
+		streamIn.writeLong(publicationTime);
+		streamIn.writeObject(channelName);
+		streamIn.writeObject(message);
+		streamIn.writeInt(type == Type.DELAYED ? 0 : 1);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("DelayedMessage[time=%s, type=%s, text=\"%s\"]", publicationTime, type, message);
+	}
+
+	public enum Type {
+		SCHEDULED, DELAYED
+	}
+
+}
--- src/dzuchun/kyobot/KyoBot.java
+++ src/dzuchun/kyobot/KyoBot.java
@@ -5,10 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 import javax.annotation.Nullable;
 import javax.security.auth.login.LoginException;
 
@@ -24,7 +24,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
@@ -52,7 +51,67 @@ public class KyoBot extends ListenerAdapter {
 	private static final SavedArrayList<ChannelName> CHANNEL_NAMES = new SavedArrayList<ChannelName>(
 			new File("channel_names.txt"), ChannelName.class);
 
-	public static void main(String[] args) throws InterruptedException, IOException {
+	private static final SavedArrayList<DelayedMessage> DELAYED_MESSAGES = new SavedArrayList<>(
+			new File("delayed_messages.txt"), DelayedMessage.class);
+	private static final Object DELAYED_MESSAGES_LOCK = new Object();
+	private static final Long DEFAULT_WAIT_TIME = 60000l;
+
+	static class DelayedMessagesThread extends Thread {
+		private Object MONITOR;
+
+		public boolean keepRunning = true;
+
+		{
+			this.setName("DelMessages-Thread");
+			this.setDaemon(true);
+		}
+
+		@Override
+		public void run() {
+			MONITOR = new Object();
+			while (keepRunning) {
+				LOGGER.debug("tick!");
+				long currentTime = System.currentTimeMillis();
+				long sleepTime = DEFAULT_WAIT_TIME;
+				List<DelayedMessage> expired = new ArrayList<>();
+				for (DelayedMessage delM : DELAYED_MESSAGES) {
+					if (delM.getPublicationTime() <= currentTime) {
+						expired.add(delM);
+						String reqName = delM.getChannelName();
+						LOGGER.debug("Sending message {}:", delM);
+						int n = 0;
+						for (ChannelName chN : CHANNEL_NAMES) {
+							if (chN.getName().equals(reqName)) {
+								TextChannel channel = chN.getChannel(jda);
+								channel.sendMessage(delM.getMessage()).complete();
+								LOGGER.info("Sent to {}", channel);
+								n++;
+							}
+						}
+						LOGGER.debug("Sent to all channels named {}, {} in total", reqName, n);
+					} else if (delM.getPublicationTime() - currentTime < sleepTime) {
+						sleepTime = delM.getPublicationTime() - currentTime;
+					}
+
+				}
+				synchronized (DELAYED_MESSAGES_LOCK) {
+					DELAYED_MESSAGES.removeAll(expired);
+				}
+				try {
+					synchronized (MONITOR) {
+						LOGGER.info("Going to sleep for {}s", sleepTime / 1000);
+						MONITOR.wait(sleepTime);
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private static final DelayedMessagesThread DELAYED_MESSAGES_THREAD = new DelayedMessagesThread();
+
+	public static void main(String[] args) throws InterruptedException {
 		if (args.length < 1 || (args.length < 2 && args[0].equals("_"))) {
 			LOGGER.error("You must specify path to file with API token, or \"_\" and specify it as second argument");
 			System.exit(-1);
@@ -64,29 +123,72 @@ public class KyoBot extends ListenerAdapter {
 				TOKEN = tokenSearcher.nextLine();
 				tokenSearcher.close();
 			} else {
-				LOGGER.info("Specify API token: ");
+//				LOGGER.info("Specify API token: ");
 				TOKEN = args[1];
 			}
 			jda = JDABuilder.createDefault(TOKEN).addEventListeners(new KyoBot()).setActivity(Activity.listening("HTT"))
 					.build();
 			jda.awaitReady();
-			ADMINISTRATORS.load();
-			if (ADMINISTRATORS.isEmpty()) {
-				LOGGER.warn("No administrator specified");
+			try {
+				ADMINISTRATORS.load();
+				if (ADMINISTRATORS.isEmpty()) {
+					LOGGER.warn("No administrators specified");
+				} else {
+					LOGGER.info("Loaded administators:\n{}", ADMINISTRATORS.toString());
+				}
+			} catch (IOException e) {
+				LOGGER.error("Could not load administrators:");
+				e.printStackTrace();
+				Thread.sleep(10);
+				System.exit(-2);
 			}
-			LOGGER.info("Loaded administators:\n{}", ADMINISTRATORS.toString());
 			loadHelpString();
-			GUILD_NAMES.load();
-			LOGGER.info("Loaded guild names:\n{}", GUILD_NAMES.toString());
-			CHANNEL_NAMES.load();
-			LOGGER.info("Loaded channel names:\n{}", CHANNEL_NAMES.toString());
-		} catch (FileNotFoundException e) {
-			LOGGER.error("No file at {}", args[0]);
-			System.exit(-1);
+			try {
+				GUILD_NAMES.load();
+				if (GUILD_NAMES.isEmpty()) {
+					LOGGER.info("No guild names loaded");
+				} else {
+					LOGGER.info("Loaded guild names:\n{}", GUILD_NAMES.toString());
+				}
+			} catch (IOException e) {
+				LOGGER.error("Could not load guild names:");
+				e.printStackTrace();
+				Thread.sleep(10);
+			}
+			try {
+				CHANNEL_NAMES.load();
+				if (CHANNEL_NAMES.isEmpty()) {
+					LOGGER.info("No channel names loaded");
+				} else {
+					LOGGER.info("Loaded channel names:\n{}", CHANNEL_NAMES.toString());
+				}
+			} catch (IOException e) {
+				LOGGER.error("Could not load channel names:");
+				e.printStackTrace();
+				Thread.sleep(10);
+			}
+			try {
+				DELAYED_MESSAGES.load();
+				if (DELAYED_MESSAGES.isEmpty()) {
+					LOGGER.info("No delayed messages loaded");
+				} else {
+					LOGGER.info("Loaded delayed messages:\n{}", DELAYED_MESSAGES.toString());
+				}
+			} catch (IOException e) {
+				LOGGER.error("Could not load delayed messages:");
+				e.printStackTrace();
+				Thread.sleep(10);
+			}
+			DELAYED_MESSAGES_THREAD.start();
+			LOGGER.info("Started delayed messages thread");
 		} catch (LoginException e) {
 			LOGGER.error("Failed to log in: ");
 			e.printStackTrace();
-			System.exit(-1);
+			System.exit(-3);
+		} catch (FileNotFoundException e) {
+			LOGGER.error("File not found:");
+			e.printStackTrace();
+			System.exit(-4);
 		}
 		LOGGER.info("Kyo logged in!");
 	}
@@ -94,9 +196,9 @@ public class KyoBot extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		super.onMessageReceived(event);
-		if ((event.getChannelType() == ChannelType.PRIVATE) && !jda.getPrivateChannels().contains(event.getChannel())) {
-			LOGGER.error("Hurray, recieved message in non-existent private channel");
-		}
+//		if ((event.getChannelType() == ChannelType.PRIVATE) && !jda.getPrivateChannels().contains(event.getChannel())) {
+//			LOGGER.error("Hurray, recieved message in non-existent private channel");
+//		}
 		Message message = event.getMessage();
 		String content = message.getContentDisplay();
 		if ((content.charAt(0) != '!') || content.length() < 2) {
@@ -108,7 +210,7 @@ public class KyoBot extends ListenerAdapter {
 		command = command == null ? BotCommand.help : command;
 
 		if (command.shouldIgnore(message)) {
-			LOGGER.info("Ignoring");
+			LOGGER.debug("Ignoring");
 			return;
 		}
 
@@ -116,7 +218,7 @@ public class KyoBot extends ListenerAdapter {
 		if (response != null) {
 			event.getAuthor().openPrivateChannel().flatMap(channel -> channel.sendMessage(response)).submit();
 		} else {
-			LOGGER.info("Response is null, so I don't send it :)");
+			LOGGER.debug("Response is null, so I don't send it :)");
 		}
 	}
 
@@ -172,7 +274,7 @@ public class KyoBot extends ListenerAdapter {
 			}
 
 		},
-		aChN("!aCh <message link> <name>, example: !aCh https://discordapp.com/channels/a1/a2/a3 news, where a1, a2, a3 are IDs. Note: multiple channels can be assigned to a single name") {
+		aChN("!aCh <message link> <name>, example: !aCh https://discordapp.com/channels/a1/a2/a3 news, where a1, a2, a3 are IDs. Note:[WIP] multiple channels can be assigned to a single name") {
 
 			@Override
 			Message process(Message messageIn) {
@@ -195,7 +297,7 @@ public class KyoBot extends ListenerAdapter {
 			}
 
 		},
-		rChN("!rCh <name>, example: !rCh news. Note: multiple channels can be assigned to a single name") {
+		rChN("!rCh <name>, example: !rCh news. Note:[WIP] multiple channels can be assigned to a single name") {
 
 			@Override
 			Message process(Message messageIn) {
@@ -224,7 +326,7 @@ public class KyoBot extends ListenerAdapter {
 			}
 
 		},
-		sDel("!sDel <channel name> <message link> <delay>, example: !sDel news https://discordapp.com/channels/a1/a2/a3 10, where a1, a2, a3 are IDs.") {
+		sDel("!sDel <channel name> <message link> <delay>, example: !sDel news https://discordapp.com/channels/a1/a2/a3 10, where a1, a2, a3 are IDs. WARN: known bug, links to messages from private chats are not recognised.") {
 
 			@Override
 			Message process(Message messageIn) {
@@ -255,17 +357,56 @@ public class KyoBot extends ListenerAdapter {
 				StringBuilder messageBuilder = new StringBuilder()
 						.append(String.format("Delayed message from %s:\n", messageIn.getAuthor().getAsMention()));
 				messageBuilder.append(coords.message.getContentDisplay());
-				channel.sendMessage(messageBuilder.toString()).submitAfter(delay, TimeUnit.SECONDS); // TODO repair
-																										// mentions
-
 				String response = String.format("Sending delayed message %s to channel %s with delay %s",
 						coords.message, channel, delay);
+				Message message = new MessageBuilder().append(response).build();
+//				channel.sendMessage(messageBuilder.toString()).submitAfter(delay, TimeUnit.SECONDS); // TODO repair mentions
+				synchronized (DELAYED_MESSAGES) {
+					DELAYED_MESSAGES.add(new DelayedMessage(System.currentTimeMillis() + delay, words[1],
+							messageBuilder.toString(), DelayedMessage.Type.DELAYED));
+				}
+				synchronized (DELAYED_MESSAGES_THREAD.MONITOR) {
+					DELAYED_MESSAGES_THREAD.MONITOR.notifyAll();
+				}
 				LOGGER.debug(response);
-				return new MessageBuilder().append(response).build();
+				return message;
 			}
 
 		},
-		sSch("!sSch <channel name> <message link> <date>, example: !sDel news https://discordapp.com/channels/a1/a2/a3 2020/04/02_07:45:34, where a1, a2, a3 are IDs.") {
+		rDel("!rDel [number of delayed message in list]") {
+
+			@Override
+			Message process(Message messageIn) {
+				String[] words = getWords(messageIn);
+				MessageBuilder builder = new MessageBuilder();
+				if (DELAYED_MESSAGES.isEmpty()) {
+					builder.append("No delayed messages");
+				} else {
+					if (words.length < 2) {
+						int i = 1;
+						builder.append("Displaying list of delayed messages:\n");
+						for (DelayedMessage delM : DELAYED_MESSAGES) {
+							builder.append(String.format("%s) %s\n", i, delM.toString()));
+						}
+					} else {
+						try {
+							int number = Integer.parseInt(words[1]);
+							DelayedMessage dMes = DELAYED_MESSAGES.get(number - 1);
+							DELAYED_MESSAGES.remove(dMes);
+							builder.append("Removed delayed message:\n").append(dMes);
+						} catch (NumberFormatException e) {
+							builder.append("Could not resolve number. Syntax:\n" + syntax);
+						} catch (IndexOutOfBoundsException e) {
+							builder.append(
+									String.format("Please, specify number from 1 to %s", DELAYED_MESSAGES.size()));
+						}
+					}
+				}
+				return builder.build();
+			}
+
+		},
+		sSch("!sSch <channel name> <message link> <date>, example: !sDel news https://discordapp.com/channels/a1/a2/a3 2020/04/02_07:45:34, where a1, a2, a3 are IDs. WARN: known bug, links to messages from private chats are not recognised.") {
 
 			@Override
 			Message process(Message messageIn) {
@@ -295,12 +436,19 @@ public class KyoBot extends ListenerAdapter {
 				builder.append(String.format("Scheduled message from %s:\n", messageIn.getAuthor().getAsMention()));
 				builder.append(coords.message.getContentDisplay());
 				long delay = date.getTime() - System.currentTimeMillis();
-				channel.sendMessage(builder.toString()).submitAfter(delay, TimeUnit.MILLISECONDS); // TODO repair
-																									// mentions
 				String response = String.format("Sending scheduled message %s to channel %s at date %s (in %s seconds)",
 						coords.message, channel, date.toString(), delay / 1000);
+				Message message = new MessageBuilder().append(response).build();
+//				channel.sendMessage(builder.toString()).submitAfter(delay, TimeUnit.MILLISECONDS); // TODO repair mentions
+				synchronized (DELAYED_MESSAGES_LOCK) {
+					DELAYED_MESSAGES.add(new DelayedMessage(date.getTime(), words[1], builder.toString(),
+							DelayedMessage.Type.SCHEDULED));
+				}
+				synchronized (DELAYED_MESSAGES_THREAD.MONITOR) {
+					DELAYED_MESSAGES_THREAD.MONITOR.notifyAll();
+				}
 				LOGGER.debug(response);
-				return new MessageBuilder().append(response).build();
+				return message;
 			}
 
 		},
@@ -329,25 +477,42 @@ public class KyoBot extends ListenerAdapter {
 			Message process(Message messageIn) {
 				messageIn.getAuthor().openPrivateChannel().flatMap(
 						channel -> channel.sendMessage(new MessageBuilder().append("K, shutting down...").build()))
-						.complete(); // TODO change this
+						.complete(); // TODO
+										// change
+										// this
 				try {
 					KyoBot.ADMINISTRATORS.save();
+					LOGGER.info("Saved admins");
 				} catch (IOException e) {
 					LOGGER.error("Failed to save admins:");
 					e.printStackTrace();
 				}
 				try {
 					KyoBot.GUILD_NAMES.save();
+					LOGGER.info("Saved guild names");
 				} catch (IOException e) {
 					LOGGER.error("Failed to save guild names:");
 					e.printStackTrace();
 				}
 				try {
 					KyoBot.CHANNEL_NAMES.save();
+					LOGGER.info("Saved channel names");
 				} catch (IOException e) {
 					LOGGER.error("Failed to save channel names:");
 					e.printStackTrace();
 				}
+				try {
+					KyoBot.DELAYED_MESSAGES.save();
+					LOGGER.info("Saved delayed messages");
+				} catch (IOException e) {
+					LOGGER.error("Failed to save delayed messages:");
+					e.printStackTrace();
+				}
+				DELAYED_MESSAGES_THREAD.keepRunning = false;
+				synchronized (DELAYED_MESSAGES_THREAD.MONITOR) {
+					DELAYED_MESSAGES_THREAD.MONITOR.notifyAll();
+				}
+				LOGGER.info("Stopped DelMessages-Thread");
 				JDA jda = KyoBot.jda;
 				jda.shutdownNow();
 				LOGGER.info("Kyo wanna sleep...");
